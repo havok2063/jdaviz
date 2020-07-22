e1489f34a19cc17db5caa3bb0910185a61afe41d
Brian Cherinka <havok2063@hotmail.com>
2020-07-22 17:34:15 -0400

    switch to using specutils identify_spectrum_format and ndim to guess valid configuration; removed classes

--- jdaviz/core/data_formats.py
+++ jdaviz/core/data_formats.py
@@ -12,130 +12,100 @@
 
 
 from __future__ import print_function, division, absolute_import
-from functools import wraps
 
-from astropy.table import Table
+import os
+import pathlib
+
+import astropy.io
 from specutils.io.registers import identify_spectrum_format
+from specutils import Spectrum1D
 
-import jdaviz
-#import jdaviz.core.data_identifiers as dataid
 from jdaviz.core.config import list_configurations
 from jdaviz.core.events import DataPromptMessage
 
 
-default_file_to_config_mapping = {'JWST x1d': 'specviz', 'JWST s2d': 'imviz', 'JWST s3d': 'cubeviz',
-                                  'MaNGA cube': 'cubeviz'}
+# create a default file format to configuration mapping
+default_mapping = {'JWST x1d': 'specviz', 'JWST s2d': 'imviz', 'JWST s3d': 'cubeviz',
+                   'MaNGA cube': 'cubeviz'}
 
-formats_table = astropy.io.registry.get_formats(data_class=specutils.Spectrum1D, readwrite='Read')
-spectral_formats = ['Spectrum1D', 'SpectrumList', 'SpectrumCollection', 'SpectralCube']
-astropy.io.registry.get_formats(readwrite='Read')
+formats_table = astropy.io.registry.get_formats(data_class=Spectrum1D, readwrite='Read')
 
+file_to_config_mapping = {i: default_mapping.get(
+    i, 'specviz') for i in formats_table['Format']}
 
-class Base(object):
-    @classmethod
-    def register(cls):
-        '''register the object in the format table '''
-        return (cls.fname, cls.config, cls.dimension, cls.identifier)
+# default n-dimension to configuration mapping
+ndim_to_config_mapping = {1: 'specviz', 2: 'imviz', 3: 'cubeviz'}
 
 
-class JWSTx1D(Base):
-    fname = 'JWST x1d'
-    config = 'specviz'
-    dimension = '1d'
-    identifier = 'identify_jwst_x1d_fits'
+def guess_dimensionality(filename):
+    ''' Guess the dimensionality of a file
 
+    Parameters
+    ----------
+    filename : str
+        The filename of the loaded data
 
-class BaseImage(Base):
-    config = 'imviz'
-    dimension = '2d'
+    Returns
+    -------
+    ndim : int
+        The number of dimensions of the data
+    '''
 
+    # check for valid string input
+    if not isinstance(filename, (str, pathlib.Path)) or not os.path.isfile(filename):
+        raise ValueError(f'{filename} is not a valid string path to a file')
 
-class JWSTs2D(BaseImage):
-    fname = 'JWST s2d'
-    config = 'imviz'
-    dimension = '2d'
-    identifier = 'identify_jwst_s2d_fits'
-
-
-class BaseCube(Base):
-    config = 'cubeviz'
-    dimension = '3d'
-
-
-class JWSTs3D(BaseCube):
-    fname = 'JWST s3d'
-    identifier = 'identify_jwst_s3d_fits'
-
-
-class MangaCube(BaseCube):
-    fname = 'MaNGA cube'
-    identifier = 'identify_sdss_manga_cube_fits'
-
-
-# generic formats
-
-class GenericCube(BaseCube):
-    fname = 'generic cube'
-    identifier = 'identify_generic_cube'
-
-
-class GenericImage(BaseImage):
-    fname = 'generic image'
-    identifier = 'identify_generic_fits_image'
-
-
-def _get_subclasses(base):
-    ''' Recursively get all subclasses from a given base class '''
-    subs = []
-    for cls in base.__subclasses__():
-        if cls.__subclasses__():
-            subs.extend(_get_subclasses(cls))
-        elif hasattr(cls, 'fname'):
-            subs.append(cls)
-        else:
-            pass
-    return subs
-
-
-def get_formats():
-    ''' Retrieve the registered list of valid jdaviz formats '''
-    tt = Table(names=['format', 'config', 'dimension', 'identifier'],
-               dtype=['str', 'str', 'str', 'str'])
-
-    for cls in _get_subclasses(Base):
-        tt.add_row(cls.register())
-
-    tt.sort('format')
-
-    return tt
+    # get the dimension number of the first extension
+    ndim = astropy.io.fits.getval(filename, 'NAXIS', ext=1)
+    return ndim
 
 
 def get_valid_format(filename):
-    ''' Identify a best match jdaviz format from a filename '''
+    ''' Identify a best match jdaviz configuration from a filename
 
-    # get a list of all valid formats
-    formats = get_formats()
+    Parameters
+    ----------
+    filename : str
+        The filename of the loaded data
 
-    # identify first available matching format
-    valid_format = None
-    config = None
-    for row in formats:
-        # lookup and call the identifier function
-        assert hasattr(dataid, row["identifier"]), (f'Identifier {row["identifier"]} missing '
-                                                    'definition in from list of functions.')
+    Returns
+    -------
+        valid_format : str
+            A valid file format
+        config : str
+            The recommended application configuration
+    '''
 
-        idfxn = getattr(dataid, row["identifier"])
-        if idfxn(filename):
-            valid_format = row['format']
-            config = row['config']
-            break
+    valid_file_format = identify_spectrum_format(filename)
+    ndim = guess_dimensionality(filename)
 
-    # return the format and config
-    return valid_format, config
+    if valid_file_format:
+        recommended_config = file_to_config_mapping.get(valid_file_format, 'default')
+    else:
+        recommended_config = ndim_to_config_mapping.get(ndim, 'default')
+
+    return valid_file_format, recommended_config
 
 
 def identify_data(filename, current=None):
-    ''' Identify the data format and application configuration from a filename '''
+    ''' Identify the data format and application configuration from a filename
+
+    Parameters
+    ----------
+    filename : str
+        The filename of the loaded data
+    current : str
+        The currently loading application configuration, if any
+
+    Returns
+    -------
+        valid_format : str
+            A valid file format
+        config : str
+            The recommended application configuration
+        status : str
+            A status message
+    '''
 
     valid_format, config = get_valid_format(filename)
 
@@ -158,38 +128,29 @@ def identify_data(filename, current=None):
     return valid_format, config, status
 
 
-def prompt_data(func):
-    ''' Decorator to identity valid data format and prompt dialog '''
+def prompt_data(app, filename):
+    ''' Prompt for a data dialog modal
 
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        inst = args[0]
-        filename = args[1]
-        if isinstance(inst, jdaviz.core.helpers.ConfigHelper):
-            app = inst.app
-            current_config = inst._default_configuration
-        else:
-            app = inst
-            current_config = app.get_configuration().get(
-                'settings').get('configuration', 'default')
+    Given a JDAviz application instance and a data filename, checks
+    for a valid file format and recommended configuration and triggers
+    the data prompt dialog window with a message.
 
-        valid_format, config, status = identify_data(filename, current=current_config)
+    Parameters
+    ----------
+    app : application instance
+        The current jdaviz application
+    filename : str
+        The filename of the loaded data
 
-        if 'success' in status.lower():
-            return func(*args, **kwargs)
+    '''
+    # get the current configuration
+    current_config = app.get_configuration().get('settings').get('configuration', 'default')
 
-        msg = DataPromptMessage(status=status, data_format=valid_format, config=config,
-                                current=current_config, sender=app)
-        app.hub.broadcast(msg)
-        #loaded = app.state.data_prompt.get('load', None)
+    # get a valid file format, config, and status
+    valid_format, config, status = identify_data(filename, current=current_config)
 
-        return func(*args, **kwargs)
-        # if loaded:
-        #     return func(*args, **kwargs)
-        # else:
-        #     print('cannot show data. exiting')
-        #     #raise ValueError('cannot show data')
-        #     return None
-
-    return wrapper
+    # broadcast a message
+    msg = DataPromptMessage(status=status, data_format=valid_format, config=config,
+                            current=current_config, sender=app)
+    app.hub.broadcast(msg)
 
